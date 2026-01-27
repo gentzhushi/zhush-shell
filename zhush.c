0bf81a816000a283fac94003b14c1050c3727dd1
Gent Zhushi <132105731+gentzhushi@users.noreply.github.com>
2026-01-27 18:57:05 +0100

    fork

--- zhush.c
+++ zhush.c
@@ -1,9 +1,34 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int evaluate(char* line);
 
 int main(int argc, char** argv){
-       
-        for (int i = 0; i < argc; i++){
-                printf("argumenti %d: %s\n", i, *(argv+i));
+      
+        printf("Shell started.\n\n");
+
+        char user_input[256];
+
+        while(1){
+                scanf(" %255[^\n]", user_input);
+                printf("Duke evaluu: %s\n\n", user_input);
+                evaluate(user_input);
+        }
+
+        return 0;
+}
+
+int evaluate(char *line){
+        pid_t pid = fork();
+
+        if (pid == 0){
+                printf("- Hej babi\n");
+                _exit(0);
+        }
+        else{
+                waitpid(pid, 0, 0);
+                printf("+ Hë bre djali jem?! A s'pe sheh qe jom nxan tu lu kumor?!?!? AAAA?!!?!?\n");
         }
 
         return 0;
